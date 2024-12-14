49ffcd43e1e53c4f685722da2c5aa9f9932c3d18
Masa Grubor <grubor.masa@gmail.com>
2024-12-14 21:03:13 +0100

    updated migrations, created database

---
+++ database/migrations/2024_12_14_192833_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->foreignId('sport_id')->constrained('sports')->onDelete('cascade');
+            $table->string('location');
+            $table->integer('max_participants');
+            $table->timestamp('start_time')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
